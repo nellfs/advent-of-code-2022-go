06b8f28561b58f48fdfd6e3649389a6c2912168a
nellfs <nellfs@localhost.localdomain>
2023-06-04 11:02:46 -0300

    final

--- 02/main.go
+++ 02/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -11,11 +12,38 @@ func main() {
 	defer input.Close()
 	inputScanner := bufio.NewScanner(input)
 
-	var score int
-	scores := map[string]int{"Rock Paper": 1, "C Y": 2, "A Z": 3, "A X": 4, "B Y": 5, "C Z": 6, "C X": 7, "A Y": 8, "B Z": 9}
-	for inputScanner.Scan() {
-		score += scores[inputScanner.Text()]
+	options := map[string]string{
+		"A": "rock",
+		"B": "paper",
+		"C": "scissors",
+		"X": "rock",
+		"Y": "paper",
+		"Z": "scissors",
 	}
 
+	var score int
+	for inputScanner.Scan() {
+		match := strings.Split(inputScanner.Text(), " ")
+		if match[0] != "" {
+			opponent, own := options[match[0]], options[match[1]]
+			score += PlayRockPaperScissors(opponent, own)
+		}
+	}
 	fmt.Println(score)
+}
+
+func PlayRockPaperScissors(opponentChoice, ownChoice string) int {
+	points := map[string]int{
+		"rock":     1,
+		"paper":    2,
+		"scissors": 3,
+	}
+
+	if ownChoice == opponentChoice {
+		return 3 + points[ownChoice]
+	} else if (points[ownChoice]-points[opponentChoice]+3)%3 == 1 {
+		return 6 + points[ownChoice]
+	} else {
+		return 0 + points[ownChoice]
+	}
 }
