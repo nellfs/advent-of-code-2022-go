0adc1f77a9b5b3f91e757d92ae338f7358167d5f
nellfs <nellfs@localhost.localdomain>
2023-06-04 00:41:06 -0300

    first

--- 01/main.go
+++ 01/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
 func main() {
 
+	input, _ := os.Open("input")
+	inputScanner := bufio.NewScanner(input)
+
+	maxCalories := 0
+	currentElfCalories := 0
+
+	for inputScanner.Scan() {
+		snack, err := strconv.Atoi(inputScanner.Text())
+		currentElfCalories += snack
+
+		if err != nil {
+			if currentElfCalories > maxCalories {
+				maxCalories = currentElfCalories
+			}
+
+			currentElfCalories = 0
+		}
+	}
+	fmt.Println(maxCalories)
+
+	input.Close()
 }
